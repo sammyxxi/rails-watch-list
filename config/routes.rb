fa7d48642e0d2664de6abc0f7d4f968bb9818092
Sammy Kavanagh <samkav025@gmail.com>
2022-11-11 11:02:43 +0900

    watch list

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resources :lists, only: [:index, :new, :create, :show] do
     resources :bookmarks, only: [:new, :create]
   end
-  resources :bookmarks, only: [:destory]
+  resources :bookmarks, only: :destory
 end
