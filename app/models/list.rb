0d7cd1cabda8565a77b1415eec0af94758c0afa6
Sammy Kavanagh <samkav025@gmail.com>
2022-11-11 16:51:40 +0900

    watch list

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,5 @@ class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, dependent: :destroy, through: :bookmarks
   validates :name, presence: true, uniqueness: true
+  has_one_attached :photo
 end
